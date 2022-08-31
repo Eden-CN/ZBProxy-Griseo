d7feed0780a4faae6571ef17537daa2f3e13ce14
格蕾修 <101780750+LittleGriseo@users.noreply.github.com>
2022-08-31 21:17:53 +0800

    Update handler.go

--- service/handler.go
+++ service/handler.go
@@ -2,10 +2,10 @@ package service
 
 import (
 	"fmt"
-	"github.com/layou233/ZBProxy/config"
-	"github.com/layou233/ZBProxy/service/minecraft"
-	"github.com/layou233/ZBProxy/service/tls"
-	"github.com/layou233/ZBProxy/service/transfer"
+	"github.com/LittleGriseo/GriseoProxy/config"
+	"github.com/LittleGriseo/GriseoProxy/service/minecraft"
+	"github.com/LittleGriseo/GriseoProxy/service/tls"
+	"github.com/LittleGriseo/GriseoProxy/service/transfer"
 	"log"
 	"net"
 )
@@ -14,8 +14,8 @@ func newConnReceiver(s *config.ConfigProxyService,
 	conn *net.TCPConn,
 	options *transfer.Options) {
 
-	log.Println("Service", s.Name, ": A new connection request sent by", conn.RemoteAddr().String(), "is received.")
-	defer log.Println("Service", s.Name, ": A connection with", conn.RemoteAddr().String(), "is closed.")
+	log.Println("服务", s.Name, ": 一个新的连接请求由", conn.RemoteAddr().String(), "接受。")
+	defer log.Println("服务", s.Name, ": 一个连接", conn.RemoteAddr().String(), "关闭.")
 	var err error // in order to avoid scoop problems
 	var remote net.Conn = nil
 
@@ -36,7 +36,7 @@ func newConnReceiver(s *config.ConfigProxyService,
 	if remote == nil {
 		remote, err = options.Out.Dial("tcp", fmt.Sprintf("%v:%v", s.TargetAddress, s.TargetPort))
 		if err != nil {
-			log.Printf("Service %s: Failed to dial to target server: %v", s.Name, err.Error())
+			log.Printf("服务 %s: 拨号到目标服务器失败: %v", s.Name, err.Error())
 			conn.Close()
 			return
 		}
